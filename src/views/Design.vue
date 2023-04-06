221b0fd41f479a7300bdbc64a4f7b980c3a8f88f
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-04-06 10:55:42 +0800

    feat: update

---
+++ src/views/Design.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-screen h-screen overflow-hidden bg-gray-500 flex justify-center relative">
+    <span class="text-sm text-slate-950">背景：<span class=" text-red-500">750 * (自适应手机高度，最小1181) </span>建议用简单纯色或图形，在小屏手机中无法完全展示</span>
+    <div class="w-full h-[1181px] bg-gray-700 absolute top-0 bottom-0 m-auto left-0 opacity-40 flex items-center justify-center">
+			内容区域：<span class=" text-red-500">750 * 1181</span>微信小屏手机最小适配高度
+		</div>
+  </div>
+</template>
